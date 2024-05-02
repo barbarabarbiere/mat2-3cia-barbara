6d5e11ab8cbc51075a5ff96dee62dd069a576836
barbarabarbiere <115155413+barbarabarbiere@users.noreply.github.com>
2024-05-02 17:20:47 -0300

    bah

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const botoes= document.querySelectorAll(".botao");
 const textos=document.querySelectorAll("aba-conteudo");
 
-for(let i=0i<botoes.length;i++){  
-  botoes[i].onclick =function(){
+for(let i=0i <botoes.length;i++){  
+  botoes[i].onclick = function(){
 
     for(let j=0;j<botoes.length;j++){
          botoes[j].classList.remove("ativo");
