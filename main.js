f36f0aff6b95a7e9a965a30e385c20b2c0b73d09
barbarabarbiere <115155413+barbarabarbiere@users.noreply.github.com>
2024-05-23 17:07:52 -0300

    bah

--- main.js
+++ main.js
@@ -43,6 +43,7 @@ function calculaTempo(tempoObjetivo) {
 }
 
 function atualizaCronometro(){
+       
     for (let i=0; i<contadores.length;i++){
         contadores[i].textContent = calculaTempo(tempos[i]);   
     }
@@ -53,4 +54,4 @@ function comecaCronometro(){
     setInterval(atualizaCronometro,1000);
 }
 
-comecaCronometro();
+//comecaCronometro();
